8ab9f5d9f950243c8eb188bc33fe5c440693f766
Scott <wsc@iri-labs.com>
2018-06-06 21:42:53 +0200

    remove version file, use semver and git tags instead.

--- version.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2016 The Gini Authors. All rights reserved.  Use of this source
-// code is governed by a license that can be found in the License file.
-
-package gini
-
-// The version of gini
-const Version = "v0.9.1"
